ec9c2307deb81aa41333229dc8cec5e8f43805c8
= <joshua.escamilla@selu.edu>
2018-07-08 22:47:12 -0500

    Rename parameters and add documentation to the Rose class

--- main.ts
+++ main.ts
@@ -2,28 +2,38 @@ class Rose {
     private readonly context: CanvasRenderingContext2D;
     private readonly centerX: number;
     private readonly centerY: number;
-    private readonly radius: number;
+    private readonly amplitude: number;
     private readonly k: number;
     private theta: number = 0;
-    private readonly speed: number;
+    private readonly deltaTheta: number;
     private previousX: number = 0;
     private previousY: number = 0;
 
+    /**
+     * Represents a rose, also known as a rhodonea curve.
+     * @param centerX The x value of the center of the rose.
+     * @param centerY The y value of the center of the rose.
+     * @param amplitude The length of each petal.
+     * @param k The petal coefficient. If `k` is an integer, the curve will be rose-shaped with
+     * `2k` petals if `k` is even, and `k` petals if `k` is odd.
+     * @param deltaTheta The amount in radians that the angle is increased every time the rose is
+     * rendered.
+     */
     public constructor(context: CanvasRenderingContext2D, centerX: number, centerY: number,
-                       radius: number, k: number, speed: number) {
+                       amplitude: number, k: number, deltaTheta: number) {
         this.context = context;
         this.centerX = centerX;
         this.centerY = centerY;
-        this.radius = radius;
+        this.amplitude = amplitude;
         this.k = k;
-        this.speed = speed;
+        this.deltaTheta = deltaTheta;
     }
 
     public render(): void {
         const x: number = this.centerX +
-            (Math.cos(this.k * this.theta) * Math.cos(this.theta) * this.radius);
+            (Math.cos(this.k * this.theta) * Math.cos(this.theta) * this.amplitude);
         const y: number = this.centerY +
-            (Math.cos(this.k * this.theta) * Math.sin(this.theta) * this.radius);
+            (Math.cos(this.k * this.theta) * Math.sin(this.theta) * this.amplitude);
         this.context.beginPath();
         this.context.moveTo(x, y);
         if (this.previousX !== 0 && this.previousY !== 0) {
@@ -32,7 +42,7 @@ class Rose {
         this.context.stroke();
         this.previousX = x;
         this.previousY = y;
-        this.theta += this.speed;
+        this.theta += this.deltaTheta;
     }
 }
 
@@ -45,14 +55,14 @@ window.onload = (): void => {
     const gridSizeInPixels: number = Math.min(width, height);
     const numberOfRoses: number = 5;
 
-    const radius: number = (gridSizeInPixels / numberOfRoses) * 0.5 * 0.9;
-    const speed: number = 0.1;
+    const amplitude: number = (gridSizeInPixels / numberOfRoses) * 0.5 * 0.9;
+    const deltaTheta: number = 0.05;
     const roses: Rose[] = [];
     for (let n: number = 1; n <= numberOfRoses; n++) {
         for (let d: number = 1; d <= numberOfRoses; d++) {
-            const centerX: number = radius + (n - 1) * (gridSizeInPixels / numberOfRoses);
-            const centerY: number = radius + (d - 1) * (gridSizeInPixels / numberOfRoses);
-            roses.push(new Rose(context, centerX, centerY, radius, n / d, speed));
+            const centerX: number = amplitude + (n - 1) * (gridSizeInPixels / numberOfRoses);
+            const centerY: number = amplitude + (d - 1) * (gridSizeInPixels / numberOfRoses);
+            roses.push(new Rose(context, centerX, centerY, amplitude, n / d, deltaTheta));
         }
     }
 
